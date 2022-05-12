616f89959732175bdf04ce1cde0d0bec7ed2796d
Phuc Nguyen <nguyenphuc1630@gmail.com>
2022-05-12 21:38:16 +0700

    Migrating to null safety

--- lib/src/callback_dispatcher.dart
+++ lib/src/callback_dispatcher.dart
@@ -17,10 +17,10 @@ void callbackDispatcher() {
 
   _backgroundChannel.setMethodCallHandler((MethodCall call) async {
     final List<dynamic> args = call.arguments;
-    final Function callback = PluginUtilities.getCallbackFromHandle(
+    final Function? callback = PluginUtilities.getCallbackFromHandle(
         CallbackHandle.fromRawHandle(args[0]));
     assert(callback != null);
-    final List<String> triggeringGeofences = args[1].cast<String>();
+    final List<String>? triggeringGeofences = args[1].cast<String>();
     final List<double> locationList = <double>[];
     // 0.0 becomes 0 somewhere during the method call, resulting in wrong
     // runtime type (int instead of double). This is a simple way to get
@@ -29,7 +29,7 @@ void callbackDispatcher() {
         .forEach((dynamic e) => locationList.add(double.parse(e.toString())));
     final Location triggeringLocation = locationFromList(locationList);
     final GeofenceEvent event = intToGeofenceEvent(args[3]);
-    callback(triggeringGeofences, triggeringLocation, event);
+    callback!(triggeringGeofences, triggeringLocation, event);
   });
   _backgroundChannel.invokeMethod('GeofencingService.initialized');
 }
