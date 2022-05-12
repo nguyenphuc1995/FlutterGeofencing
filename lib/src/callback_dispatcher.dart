b90ca87cd49f15e0a58d8e112bb9d6712e2b6765
Phuc Nguyen <nguyenphuc1630@gmail.com>
2022-05-12 22:38:38 +0700

    remove Unnecessary cast

--- lib/src/callback_dispatcher.dart
+++ lib/src/callback_dispatcher.dart
@@ -20,7 +20,7 @@ void callbackDispatcher() {
     final Function? callback = PluginUtilities.getCallbackFromHandle(
         CallbackHandle.fromRawHandle(args[0]));
     assert(callback != null);
-    final List<String>? triggeringGeofences = args[1].cast<String>();
+    final List<String> triggeringGeofences = args[1].cast<String>();
     final List<double> locationList = <double>[];
     // 0.0 becomes 0 somewhere during the method call, resulting in wrong
     // runtime type (int instead of double). This is a simple way to get
